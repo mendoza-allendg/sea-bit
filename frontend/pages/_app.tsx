312f4608d565db377c9d81b7274d237e0d9f69c2
Allen <mendoza-allendg@gmail.com>
2024-01-11 20:08:58 -0800

    Added NextUI library

---
+++ frontend/pages/_app.tsx
@@ -0,0 +1,15 @@
+import type { AppProps } from "next/app";
+
+import { NextUIProvider } from "@nextui-org/react";
+import {useRouter} from 'next/router';
+import "@/styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+	return (
+		<NextUIProvider navigate={router.push}>
+      <Component {...pageProps} />
+		</NextUIProvider>
+	);
+}
