8278cd10018af32db7125e2c80378ca561882e52
Allen <mendoza-allendg@gmail.com>
2024-01-12 19:43:00 -0800

    ADded prettier

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,15 +1,15 @@
-import type { AppProps } from "next/app";
+import type { AppProps } from 'next/app'
 
-import { NextUIProvider } from "@nextui-org/react";
-import {useRouter} from 'next/router';
-import "@/styles/globals.css";
+import { NextUIProvider } from '@nextui-org/react'
+import { useRouter } from 'next/router'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
+  const router = useRouter()
 
-	return (
-		<NextUIProvider navigate={router.push}>
+  return (
+    <NextUIProvider navigate={router.push}>
       <Component {...pageProps} />
-		</NextUIProvider>
-	);
+    </NextUIProvider>
+  )
 }
