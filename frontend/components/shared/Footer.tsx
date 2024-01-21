225e2491a08b46348477876c5d8d5a4213fbb7d2
vonnerbosa <vonherbosa@gmail.com>
2024-01-20 16:07:32 -0800

    created footer component

---
+++ frontend/components/shared/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+    <div className="flex flex-wrap justify-between items-center bg-slate-600 h-16 sticky bottom-0 border-b-[1px] border-solid border-gray-300 w-full">
+      <div className="flex">
+        <ul className="font-bold"></ul>
+      </div>
+    </div>
+  )
+}
