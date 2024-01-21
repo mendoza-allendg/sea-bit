c39723f4451dc686375c7b1a50493d2f21ffc0c2
vonnerbosa <vonherbosa@gmail.com>
2024-01-20 22:11:36 -0800

    added footer contents

--- frontend/components/shared/Footer.tsx
+++ frontend/components/shared/Footer.tsx
@@ -2,25 +2,28 @@ export const Footer = () => {
   return (
     <div className="flex flex-wrap justify-between items-center bg-gray-100 h-auto sticky bottom-0 border-b-[1px] border-solid border-gray-300 w-full">
       <div className="flex justify-between text-sm gap-4">
-         <ul className="flex flex-col justify-start p-4"><span className="font-bold">Contact Us</span>
+        <ul className="flex flex-col justify-start p-4">
+          <span className="font-bold">Contact Us</span>
           <li className="list-none">Customer Support</li>
           <li className="list-none">Service Guarantee</li>
           <li className="list-none">Webste Feedback</li>
         </ul>
-        
-        <ul className="flex flex-col justify-start p-4"><span className="font-bold">About</span>
+
+        <ul className="flex flex-col justify-start p-4">
+          <span className="font-bold">About</span>
           <li className="list-none">About us</li>
           <li className="list-none">Terms and Conditions</li>
           <li className="list-none">Privacy and Cookies</li>
         </ul>
-        
-        <ul className="flex flex-col justify-start p-4"><span className="font-bold">Partner With Us</span>
+
+        <ul className="flex flex-col justify-start p-4">
+          <span className="font-bold">Partner With Us</span>
           <li className="list-none">About us</li>
           <li className="list-none">Terms and Conditions</li>
           <li className="list-none">Privacy and Cookies</li>
-        </ul>        
+        </ul>
       </div>
-      
+
       <div className="flex p-16 text-small">
         <div>Payment Methods</div>
       </div>
