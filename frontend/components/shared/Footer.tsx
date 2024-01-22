32e78eeb0f9063fe98ada23f072f833c5f2cb9f8
vonnerbosa <vonherbosa@gmail.com>
2024-01-22 02:57:52 -0800

    just forgot to yarn format

--- frontend/components/shared/Footer.tsx
+++ frontend/components/shared/Footer.tsx
@@ -1,21 +1,14 @@
 import { Image, Link } from '@nextui-org/react'
 
 const contactMethods = [
-  "Customer Support",
-  "Service Guarantee",
-  "Website Feedback"
-];
+  'Customer Support',
+  'Service Guarantee',
+  'Website Feedback',
+]
 
-const companyInfo = [
-  "About Us",
-  "Terms and Condition",
-  "Privacy and Cookies"
-];
+const companyInfo = ['About Us', 'Terms and Condition', 'Privacy and Cookies']
 
-const partnerMethods = [
-  "Partner Portal",
-  "Affiliate Program"
-];
+const partnerMethods = ['Partner Portal', 'Affiliate Program']
 
 export const Footer = () => {
   return (
@@ -23,35 +16,41 @@ export const Footer = () => {
       <div className="bg-slate-500 container px-6 lg:px-20 xl:px-35 2xl:px-14 items-start flex justify-between w-full py-7">
         <div className="bg-purple-50 flex justify-center text-xs mx-auto flex-shrink-0">
           <div className="flex flex-col justify-start p-4">
-            <span className="font-bold text-base pb-1">Contact Us</span> 
-            <ul>{contactMethods.map(contact => 
-              <li className="list-none"> 
-              <Link href="#" color='foreground' className='text-xs'>
-              {contact} 
-              </Link>
-              </li>)}
-            </ul>            
+            <span className="font-bold text-base pb-1">Contact Us</span>
+            <ul>
+              {contactMethods.map((contact) => (
+                <li className="list-none">
+                  <Link href="#" color="foreground" className="text-xs">
+                    {contact}
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </div>
 
           <div className="flex flex-col justify-start p-4">
             <span className="font-bold text-base pb-1">Company</span>
-            <ul>{companyInfo.map(info => 
-              <li className="list-none"> 
-              <Link href="#" color='foreground' className='text-xs'>
-              {info} 
-              </Link>
-              </li>)}
+            <ul>
+              {companyInfo.map((info) => (
+                <li className="list-none">
+                  <Link href="#" color="foreground" className="text-xs">
+                    {info}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
 
           <div className="flex flex-col justify-start p-4">
             <span className="font-bold text-base pb-1">Partner With Us</span>
-            <ul>{partnerMethods.map(partner => 
-              <li className="list-none"> 
-              <Link href="#" color='foreground' className='text-xs'>
-              {partner} 
-              </Link>
-              </li>)}
+            <ul>
+              {partnerMethods.map((partner) => (
+                <li className="list-none">
+                  <Link href="#" color="foreground" className="text-xs">
+                    {partner}
+                  </Link>
+                </li>
+              ))}
             </ul>
           </div>
         </div>
@@ -59,65 +58,65 @@ export const Footer = () => {
         <div className="bg-red-950 flex flex-col p-4 text-sm justify-center mx-auto text-gray-500 flex-shrink max-w-md">
           <div>Payment Methods</div>
           {/*<div className="flex flex-wrap">*/}
-            <div className="flex flex-wrap justify-center">
-              <Image
-                src={'/payment_logos/visa.png'}
-                className="mt-1 w-14 rounded-lg border "
-              />
-              <Image
-                src={'/payment_logos/mastercard.png'}
-                className="mt-1 w-14 rounded-lg border "
-              />
-              <Image
-                src={'/payment_logos/Gcash.png'}
-                className="mt-1 w-14 rounded-lg border "
-              />
-              <Image
-                src={'/payment_logos/GrabPay.png'}
-                className="mt-1 w-14 rounded-lg border "
-              />
-              <Image
-                src={'/payment_logos/PayMaya.png'}
-                className="mt-1 w-14 rounded-lg border "
-              />
-              <Image
-                src={'/payment_logos/BPI.png'}
-                className="mt-1 w-14 rounded-lg border "
-              />
-              <Image
-                src={'/payment_logos/SecurityBank.png'}
-                className="mt-1 w-14 rounded-lg border "
-              />
-              <Image
-                src={'/payment_logos/AUB.png'}
-                className="mt-1 w-14 rounded-lg border "
-              />
-              <Image
-                src={'/payment_logos/BillEase.png'}
-                className="mt-1 w-14 rounded-lg border "
-              />
-              <Image
-                src={'/payment_logos/CoinsPH.png'}
-                className="mt-1 w-14 rounded-lg border "
-              />
-              <Image
-                src={'/payment_logos/7_eleven.png'}
-                className="mt-1 w-14 rounded-lg border "
-              />
-              <Image
-                src={'/payment_logos/CebuanaLL.png'}
-                className="mt-1 w-14 rounded-lg border "
-              />
-              <Image
-                src={'/payment_logos/M_LL.png'}
-                className="mt-1 w-14 rounded-lg border "
-              />
-              <Image
-                src={'/payment_logos/Landbank.jpg'}
-                className="mt-1 w-14 rounded-lg border "
-              />
-            </div>
-{/*
+          <div className="flex flex-wrap justify-center">
+            <Image
+              src={'/payment_logos/visa.png'}
+              className="mt-1 w-14 rounded-lg border "
+            />
+            <Image
+              src={'/payment_logos/mastercard.png'}
+              className="mt-1 w-14 rounded-lg border "
+            />
+            <Image
+              src={'/payment_logos/Gcash.png'}
+              className="mt-1 w-14 rounded-lg border "
+            />
+            <Image
+              src={'/payment_logos/GrabPay.png'}
+              className="mt-1 w-14 rounded-lg border "
+            />
+            <Image
+              src={'/payment_logos/PayMaya.png'}
+              className="mt-1 w-14 rounded-lg border "
+            />
+            <Image
+              src={'/payment_logos/BPI.png'}
+              className="mt-1 w-14 rounded-lg border "
+            />
+            <Image
+              src={'/payment_logos/SecurityBank.png'}
+              className="mt-1 w-14 rounded-lg border "
+            />
+            <Image
+              src={'/payment_logos/AUB.png'}
+              className="mt-1 w-14 rounded-lg border "
+            />
+            <Image
+              src={'/payment_logos/BillEase.png'}
+              className="mt-1 w-14 rounded-lg border "
+            />
+            <Image
+              src={'/payment_logos/CoinsPH.png'}
+              className="mt-1 w-14 rounded-lg border "
+            />
+            <Image
+              src={'/payment_logos/7_eleven.png'}
+              className="mt-1 w-14 rounded-lg border "
+            />
+            <Image
+              src={'/payment_logos/CebuanaLL.png'}
+              className="mt-1 w-14 rounded-lg border "
+            />
+            <Image
+              src={'/payment_logos/M_LL.png'}
+              className="mt-1 w-14 rounded-lg border "
+            />
+            <Image
+              src={'/payment_logos/Landbank.jpg'}
+              className="mt-1 w-14 rounded-lg border "
+            />
+          </div>
+          {/*
             <div className="flex justify-center">
               <Image
                 src={'/payment_logos/AUB.png'}
