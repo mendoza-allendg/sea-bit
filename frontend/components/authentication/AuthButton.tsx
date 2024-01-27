507d8d57cd5353fe349db0f7776a410316f2d448
Allen <mendoza-allendg@gmail.com>
2024-01-26 16:44:07 -0800

    add react hook form

--- frontend/components/authentication/AuthButton.tsx
+++ frontend/components/authentication/AuthButton.tsx
@@ -13,20 +13,19 @@ import {
 import { FcGoogle } from 'react-icons/fc'
 import { FaFacebook, FaApple } from 'react-icons/fa'
 import { useRegistrationApi } from '@/apis/authentication/hooks'
-import { useEffect, useState } from 'react'
-import type { User } from '@/apis/authentication/hooks'
+import { useForm, Controller } from 'react-hook-form'
 
 export const AuthButton = () => {
-  const { onOpen, isOpen, onOpenChange } = useDisclosure()
-  const [params, setParams] = useState<User>({
-    email: undefined,
-    password: undefined,
+  const { control, handleSubmit } = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
   })
-  const { data, error, refetch: login } = useRegistrationApi(params)
 
-  useEffect(() => {
-    console.log(data)
-  }, [data])
+  const { onOpen, isOpen, onOpenChange } = useDisclosure()
+
+  const { error, mutate } = useRegistrationApi()
 
   return (
     <>
@@ -46,25 +45,38 @@ export const AuthButton = () => {
               <ModalHeader className="flex flex-col gap-1">Sign In</ModalHeader>
               {error && <span>{error.message}</span>}
               <ModalBody>
-                <Input
-                  type="email"
-                  label="Email"
-                  variant="bordered"
-                  onValueChange={(value) =>
-                    setParams({ ...params, email: value })
-                  }
+                <Controller
+                  name="email"
+                  control={control}
+                  render={({ field }) => (
+                    <Input
+                      type="email"
+                      label="Email"
+                      variant="bordered"
+                      {...field}
+                    />
+                  )}
                 />
-                <Input
-                  type="password"
-                  label="Password"
-                  variant="bordered"
-                  onValueChange={(value) =>
-                    setParams({ ...params, password: value })
-                  }
+                <Controller
+                  name="password"
+                  control={control}
+                  render={({ field }) => (
+                    <Input
+                      type="password"
+                      label="password"
+                      variant="bordered"
+                      {...field}
+                    />
+                  )}
                 />
-                <Button color="primary" size="lg" onClick={() => login()}>
+                <Button
+                  color="primary"
+                  size="lg"
+                  onClick={handleSubmit((data) => mutate(data))}
+                >
                   Continue
                 </Button>
+
                 <div className="flex w-auto flex-1 items-center justify-center">
                   <Divider className=" w-1/4" />
                   <span className="mx-4 my-4">Or Continue With</span>
