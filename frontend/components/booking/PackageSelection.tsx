82f4c41dfa4534d2a2ddae10d74281bd7726a717
Allen <mendoza-allendg@gmail.com>
2024-02-06 01:54:37 -0800

    fixed format

--- frontend/components/booking/PackageSelection.tsx
+++ frontend/components/booking/PackageSelection.tsx
@@ -1,9 +1,8 @@
-
-import React, { ReactNode } from "react";
-import {RadioGroup, useRadio, VisuallyHidden, cn} from "@nextui-org/react";
+import React, { ReactNode } from 'react'
+import { RadioGroup, useRadio, VisuallyHidden, cn } from '@nextui-org/react'
 
 type Props = {
-  description: string;
+  description: string
   value: string
   children: ReactNode
   price: string
@@ -19,17 +18,17 @@ export const CustomRadio = (props: Props) => {
     getLabelProps,
     getLabelWrapperProps,
     getControlProps,
-  } = useRadio(props);
+  } = useRadio(props)
 
-  const { price } = props;
+  const { price } = props
 
   return (
     <Component
       {...getBaseProps()}
       className={cn(
-        "group inline-flex items-center hover:opacity-70 active:opacity-50 justify-between flex-row-reverse tap-highlight-transparent",
-        "max-w-[350px] cursor-pointer border-2 border-default rounded-lg gap-4 p-4",
-        "data-[selected=true]:border-primary",
+        'group inline-flex flex-row-reverse items-center justify-between tap-highlight-transparent hover:opacity-70 active:opacity-50',
+        'max-w-[350px] cursor-pointer gap-4 rounded-lg border-2 border-default p-4',
+        'data-[selected=true]:border-primary',
       )}
     >
       <VisuallyHidden>
@@ -41,28 +40,38 @@ export const CustomRadio = (props: Props) => {
       <div {...getLabelWrapperProps()}>
         {children && <span {...getLabelProps()}>{children}</span>}
         {description && (
-          <span className="text-small text-foreground opacity-70">{description}</span>
+          <span className="text-small text-foreground opacity-70">
+            {description}
+          </span>
         )}
-        <span className="font-bold text-lg mt-4">&#8369;{price}</span>
+        <span className="mt-4 text-lg font-bold">&#8369;{price}</span>
       </div>
     </Component>
-  );
-};
+  )
+}
 
 export const PackageSelection = () => {
   return (
     <RadioGroup>
-      <div className="flex flex-col sm:flex-row gap-2">
-      <CustomRadio description="Introduction to freeediving" price="1,750" value="1">
-        <span className="font-bold text-base">Introduction</span>
-      </CustomRadio>
-      <CustomRadio description="Fun dive with coaches" price="750" value="2">
-        <span className="font-bold text-base">Fun Dive</span>
-      </CustomRadio>
-      <CustomRadio description="Stay the night for socials" price="1,300" value="1F">
-        <span className="font-bold text-base">Accomodation</span>
-      </CustomRadio>
+      <div className="flex flex-col gap-2 sm:flex-row">
+        <CustomRadio
+          description="Introduction to freeediving"
+          price="1,750"
+          value="1"
+        >
+          <span className="text-base font-bold">Introduction</span>
+        </CustomRadio>
+        <CustomRadio description="Fun dive with coaches" price="750" value="2">
+          <span className="text-base font-bold">Fun Dive</span>
+        </CustomRadio>
+        <CustomRadio
+          description="Stay the night for socials"
+          price="1,300"
+          value="1F"
+        >
+          <span className="text-base font-bold">Accomodation</span>
+        </CustomRadio>
       </div>
     </RadioGroup>
-  );
+  )
 }
