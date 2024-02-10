d3b46a43172f1af4773fc58236e15f78cadf6621
mendoza-allendg <mendoza.allendg@gmail.com>
2024-02-10 10:08:22 -0800

    created addons section

--- frontend/components/booking/PackageSelection.tsx
+++ frontend/components/booking/PackageSelection.tsx
@@ -7,7 +7,7 @@ type Props = {
   children: ReactNode
   price: string
 }
-export const CustomRadio = (props: Props) => {
+const PackageOption = (props: Props) => {
   const {
     Component,
     children,
@@ -27,7 +27,7 @@ export const CustomRadio = (props: Props) => {
       {...getBaseProps()}
       className={cn(
         'group inline-flex flex-row-reverse items-center justify-between tap-highlight-transparent hover:opacity-70 active:opacity-50',
-        'max-w-[350px] cursor-pointer gap-4 rounded-lg border-2 border-default p-4',
+        'cursor-pointer gap-4 rounded-lg border-2 border-default p-4 md:w-[200px]',
         'data-[selected=true]:border-primary',
       )}
     >
@@ -54,23 +54,27 @@ export const PackageSelection = () => {
   return (
     <RadioGroup size="sm">
       <div className="flex flex-col flex-wrap gap-2 md:flex-row">
-        <CustomRadio
+        <PackageOption
           description="Introduction to freeediving"
           price="1,750"
           value="1"
         >
           <span className="text-base font-bold">Introduction</span>
-        </CustomRadio>
-        <CustomRadio description="Fun dive with coaches" price="750" value="2">
+        </PackageOption>
+        <PackageOption
+          description="Fun dive with coaches"
+          price="750"
+          value="2"
+        >
           <span className="text-base font-bold">Fun Dive</span>
-        </CustomRadio>
-        <CustomRadio
+        </PackageOption>
+        <PackageOption
           description="Stay the night for socials"
           price="1,300"
           value="1F"
         >
           <span className="text-base font-bold">Accomodation</span>
-        </CustomRadio>
+        </PackageOption>
       </div>
     </RadioGroup>
   )
