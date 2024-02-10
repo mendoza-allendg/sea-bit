20bbc20084fd9e0b6951eb5533096b14993d93c4
mendoza-allendg <mendoza.allendg@gmail.com>
2024-02-10 10:00:53 -0800

    ADded prettier

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,12 +1,11 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
+import DefaultLayout from '@/layouts/default'
 
 export default function IndexPage() {
-	return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <text>hello</text>
-    </section>
-	);
+  return (
+    <DefaultLayout>
+      <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        <text>hello</text>
+      </section>
+    </DefaultLayout>
+  )
 }
