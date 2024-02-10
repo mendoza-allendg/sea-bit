39a6b91cb75fbf54dc1febb99745b1fc5dc0be29
mendoza-allendg <mendoza.allendg@gmail.com>
2024-02-10 10:00:41 -0800

    Added NextUI library

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code";
+import { button as buttonStyles } from "@nextui-org/theme";
+
+export default function IndexPage() {
+	return (
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+      <text>hello</text>
+    </section>
+	);
+}
