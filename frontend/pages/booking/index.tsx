c4c2abd7b96e2f958911389183bc377371962917
mendoza-allendg <mendoza.allendg@gmail.com>
2024-02-10 10:07:52 -0800

    fixed format

--- frontend/pages/booking/index.tsx
+++ frontend/pages/booking/index.tsx
@@ -22,7 +22,7 @@ export default function IndexPage() {
         </div>
         <Gallery />
         <SectionBase title={'Packages'}>
-      <PackageSelection />
+          <PackageSelection />
         </SectionBase>
       </section>
     </DefaultLayout>
