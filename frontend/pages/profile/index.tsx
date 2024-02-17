bdd78b9466fbaa0e5bbd5de9442c90d927c156ba
vonnerbosa <vonherbosa@gmail.com>
2024-02-16 16:57:05 -0800

    changed the total feel of the page, addeed accordion

--- frontend/pages/profile/index.tsx
+++ frontend/pages/profile/index.tsx
@@ -1,154 +1,82 @@
+import { SectionTitle } from '@/components/shared/typography'
 import DefaultLayout from '@/layouts/default'
-import { Link, user } from '@nextui-org/react'
-import { ifError } from 'assert';
-
-const PROFILE_MENUS = [
-  {
-    name: 'My Bookings',
-    src: '#',
-  },
-  {
-    name: 'Favourites',
-    src: '#',
-  },
-  {
-    name: 'Messages',
-    src: '#',
-  },
-  {
-    name: 'Reviews',
-    src: '#',
-  },
-  {
-    name: 'Profile',
-    src: '#',
-  },
-]
+import {
+  Accordion,
+  AccordionItem,
+  BreadcrumbItem,
+  Breadcrumbs,
+  Input,
+} from '@nextui-org/react'
 
 const user_infos = [
   {
-    name: "Kim Allen Joseph Lazo",
-    email: "kajlazo@pasoketits.com",
-    mobile: "09064595820",
-    password: "kapagako'ynagiisamadalasakongnagbabate"
-  }
+    firstname: 'Kim Allen Joseph',
+    lastname: 'Lazo',
+    email: 'kajlazo@pasoketits.com',
+    mobile: '09064595820',
+    password: "kapagako'ynagiisamadalasakongnagbabate",
+  },
 ]
 
 function maskPassword(password: string) {
-  return '•'.repeat(password.length);
+  const truncatedPassword = password.slice(0, 15)
+  return '•'.repeat(truncatedPassword.length)
 }
 
+const fullname = `${user_infos[0].firstname} ${user_infos[0].lastname}`
 
 const UPPER_PROFILE_COMPONENTS = [
   {
-    name: "Name",
-    info: user_infos[0].name
-  }, 
-  {
-    name: "Email",
-    info: user_infos[0].email
+    name: 'Name',
+    info: fullname,
+    type: 'name',
   },
   {
-    name: "Phone Number",
-    info: user_infos[0].mobile
+    name: 'Email',
+    info: user_infos[0].email,
+    type: 'email',
   },
   {
-    name: "Password",
-    info: maskPassword(user_infos[0].password)
+    name: 'Phone Number',
+    info: user_infos[0].mobile,
+    type: 'number',
+  },
+  {
+    name: 'Password',
+    info: maskPassword(user_infos[0].password),
+    type: 'password',
   },
 ]
-
-const user_cards = [{
-  cardType: "Visa",
-  cardNumber: "0000111122224444"
-}]
-
-const LOWER_PROFILE_COMPONENTS = [
-  {
-    name: "Saved Cards",
-    info: function () {
-      if (!user_cards.length) {
-        return "None"
-      } else {
-        return (
-          <div>
-            {user_cards.map((card, i) => (
-              <div key={i}>
-                <div>Card Type: {card.cardType}</div>
-                <div>Card Number: {card.cardNumber}</div>
-              </div>
-            ))}
-          </div>
-        )}}}];
-
-
-
-
-
 
 export default function ProfilePage() {
   return (
     <DefaultLayout>
       <section className="h-full w-full">
-        <div className="xl:px-35 container m-4 mx-auto flex w-full justify-center items-start rounded-lg p-5 lg:px-20 2xl:px-6">
-
-          <div className="max-h-64 min-w-56 flex-shrink-0 flex-col rounded-lg p-4 bg-gray-100 border border-gray-300 shadow-md">
-            <ul>
-              {PROFILE_MENUS.map((menu, i) => (
-                <li className="list-none p-2" key={i}>
-                  <Link
-                    href={menu.src}
-                    color="foreground"
-                    className="text-large"
-                  >
-                    {menu.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+        <div className="xl:px-35 container mx-auto flex w-full flex-col items-start justify-center rounded-lg p-5 lg:px-20 2xl:px-36">
+          <div className="container mx-auto mb-10 mt-4 flex h-auto w-full flex-col items-start rounded-lg px-4 text-slate-950 2xl:px-20">
+            <Breadcrumbs className="mb-3">
+              <BreadcrumbItem>Account</BreadcrumbItem>
+              <BreadcrumbItem>Personal Info</BreadcrumbItem>
+            </Breadcrumbs>
+            <SectionTitle text={'Personal info'}></SectionTitle>
           </div>
 
-          <div className="container mx-auto flex h-auto w-full flex-col items-start gap-2 rounded-lg px-4 text-slate-950">
-            <div className='text-2xl font-medium'>User Details</div>
+          <div className="container mx-auto mb-9 flex h-full w-full flex-col items-start gap-3 rounded-lg px-4 text-slate-950 2xl:px-20">
             {UPPER_PROFILE_COMPONENTS.map((comp, i) => (
-                <div className="flex w-full justify-between items-center bg-gray-100 border border-gray-300 px-4 py-5 shadow-md hover:bg-rose-200" key={i}>
-                  <div className='flex flex-col'>
-                    <div className='font-semibold'>{comp.name}</div>
-                    <div>{comp.info}</div>
+              <Accordion isCompact>
+                <AccordionItem
+                  className="rounded-lg border border-gray-300 bg-gray-100 px-4 py-4 shadow-md delay-75 hover:drop-shadow-xl"
+                  key={i}
+                  title={comp.name}
+                  subtitle={comp.info}
+                >
+                  <div className="flex w-full flex-wrap gap-4 md:flex-nowrap">
+                    <Input type={comp.type} label={comp.name} />
                   </div>
-                  <div className='font-semibold'>Edit</div>
-                </div>
-              ))}
-              
-            <div className='text-2xl font-medium'>Payment Methods</div>
-            {/*{LOWER_PROFILE_COMPONENTS.map((comp, i) => (
-                <div className="flex w-full justify-between items-center bg-gray-100 border border-gray-300 px-4 py-5 shadow-md" key={i}>
-                  <div className='flex flex-col'>
-                    <div className='font-semibold'>{comp.name}</div>
-                    <div>{comp.info}</div>
-                  </div>
-                  <div className='font-semibold'>Edit</div>
-                </div>
-            ))}*/}
-
-            
-            <div
-              id="parentPaymentContainer"
-              className="flex w-full justify-between"
-            >
-                <div className="flex w-full justify-between items-center bg-gray-100 border border-gray-300 px-4 py-5 shadow-md">
-                  <div className='flex flex-col'>
-                    <div className='font-semibold'>Saved Cards</div>
-                    <div>None</div>
-                  </div>
-                  <div className='font-semibold'>+Add</div>
-                </div>
-            </div>
-            <div id="deleteContainer">
-              <div>Delete my account</div>
-            </div>
-            </div>
-          
+                </AccordionItem>
+              </Accordion>
+            ))}
+          </div>
         </div>
       </section>
     </DefaultLayout>
