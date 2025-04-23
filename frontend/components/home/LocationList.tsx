07645ac17de97c0567e1bdbe823365f4aa429cd4
mendoza-allendg <mendoza-allendg@gmail.com>
2025-04-23 23:56:58 +0800

    revamped location cards

--- frontend/components/home/LocationList.tsx
+++ frontend/components/home/LocationList.tsx
@@ -2,54 +2,129 @@ import React from 'react'
 import { Card, CardHeader } from '@/components/ui/card'
 import Image from 'next/image'
 import { Button } from '../ui/button'
+import {
+  GiJellyfish,
+  GiFlatfish,
+  GiTurtle,
+  GiCoral,
+  GiShipWreck,
+} from 'react-icons/gi'
+
+type Sighting = 'turtle' | 'jellyfish' | 'shipwreck' | 'corals' | 'fish'
+
 type Location = {
   name: string
   src: string
+  country: string
+  spots: number
+  average_depth: number
+  sightings: Sighting[]
+}
+
+const sightingIcons = {
+  jellyfish: GiJellyfish,
+  fish: GiFlatfish,
+  turtle: GiTurtle,
+  corals: GiCoral,
+  shipwreck: GiShipWreck,
 }
 
 const SAMPLE_LOCATIONS: Location[] = [
   {
     name: 'Coron, Palawan',
-    src: '/locations/palawan.jpg',
+    src: '/locations/coron.jpg',
+    country: 'Philippines',
+    spots: 12,
+    average_depth: 12,
+    sightings: ['shipwreck', 'corals', 'fish'],
   },
-  // {
-  //   name: 'Apo Reef',
-  //   src: '/locations/apo_reef.jpg',
-  // },
-  // {
-  //   name: 'Batangas',
-  //   src: '/locations/batangas_masasa.jpg',
-  // },
+  {
+    name: 'Apo Reef',
+    src: '/locations/apo_reef.jpg',
+    country: 'Philippines',
+    spots: 9,
+    average_depth: 5,
+    sightings: ['jellyfish', 'corals', 'fish'],
+  },
+  {
+    name: 'Batangas',
+    src: '/locations/batangas_masasa.jpg',
+    country: 'Philippines',
+    spots: 7,
+    average_depth: 8,
+    sightings: ['turtle', 'corals', 'fish'],
+  },
   {
     name: 'Moalboal, Cebu',
     src: '/locations/cebu_sardines.jpg',
+    country: 'Philippines',
+    spots: 2,
+    average_depth: 8,
+    sightings: ['turtle', 'corals', 'fish'],
   },
   {
     name: 'Panglao, Bohol',
     src: '/locations/panglao.jpg',
+    country: 'Philippines',
+    spots: 2,
+    average_depth: 7,
+    sightings: ['corals', 'fish'],
   },
   {
     name: 'Siargao',
     src: '/locations/siargao.jpg',
+    country: 'Philippines',
+    spots: 1,
+    average_depth: 10,
+    sightings: ['corals', 'fish'],
   },
 ]
 
 const LocationCard = ({ location }: { location: Location }) => {
-  const { name, src } = location
+  const { name, src, country, spots, average_depth, sightings } = location
 
   return (
-    <Card className="relative h-[250px] w-full">
-      <CardHeader className="absolute top-1 z-10 flex-col !items-start">
-        <p className="font-bold uppercase text-white/60">New!</p>
-        <p className="text-lg font-bold text-white">{name}</p>
-      </CardHeader>
+    <Card className="relative h-[280px] w-full">
       <Image
         alt={name}
-        className="z-0 h-full w-full rounded-md object-cover"
+        className="aboslute z-0 h-full w-full rounded-md object-cover"
         src={src}
         width={500}
         height={500}
       />
+      <div className="absolute right-5 top-2 flex flex-col items-center leading-tight">
+        <p className="text-lg font-bold text-white text-shadow-md">{spots}</p>
+        <p className="-mt-1 text-sm font-bold text-white text-shadow-md">
+          spots
+        </p>
+      </div>
+      <div className="absolute top-0 flex h-full w-full flex-col items-center justify-center text-center">
+        <p className="text-2xl font-bold text-white text-shadow-md">{name}</p>
+        <p className="text-sm font-bold text-white text-shadow-md">{country}</p>
+      </div>
+      <div className="absolute bottom-2 left-5 right-5 flex justify-between">
+        <div className="items-left flex flex-col">
+          <p className="text-lg font-bold text-white text-shadow-md">
+            {average_depth}m
+          </p>
+          <p className="text-xs font-bold text-white text-shadow-md">
+            avg depth
+          </p>
+        </div>
+        <div className="items-right flex flex-col text-right">
+          <div className="flex flex-row items-center">
+            {sightings.map((sighting) => {
+              const Icon = sightingIcons[sighting]
+              return (
+                <Icon size={30} color={'white'} className="drop-shadow-md" />
+              )
+            })}
+          </div>
+          <p className="text-sm font-bold text-white text-shadow-md">
+            sightings
+          </p>
+        </div>
+      </div>
     </Card>
   )
 }
