113d1694eab65e8b33656411e5a371b31cfe4486
mendoza-allendg <mendoza-allendg@gmail.com>
2025-04-23 23:57:08 +0800

    removed search bart

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -6,9 +6,7 @@ export default function IndexPage() {
   return (
     <DefaultLayout>
       <section className="p-b-8 md:p-b-10 flex flex-col items-center justify-center gap-4">
-        <div className="flex min-h-[28rem] w-screen items-center justify-center rounded-b-md bg-[url('/landing-page-image.jpg')] bg-cover">
-          <SearchBar />
-        </div>
+        <div className="flex min-h-[28rem] w-screen items-center justify-center rounded-b-md bg-[url('/landing-page-image.jpg')] bg-cover"></div>
         <SectionBase title={'Where to?'}>
           <LocationList />
         </SectionBase>
